8375e536a0af0dd92e4ab182d553509633fc555a
Ednilson Graciano dos Santos <EddieSantos@users.noreply.github.com>
2025-03-02 01:12:00 +0000

    Desafio: Nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,10 +9,10 @@
     Curso ADS - Introdução à Programação de Computadores
     Tema 1 - Super Trunfo em C - Fundamentos e Técnicas Avançadas
     Exercício: Desafio Nível Novato - Criando as Cartas do Super Trunfo
-    Data: 28 de Fevereiro de 2025
+    Data: 01 de Março de 2025
     Aluno: Ednilson Graciano dos Santos
     Matrícula: 202503249997 
-    Versão: 0.1.5v
+    Versão: 0.2.0v
 */
 
 int main() {
@@ -33,13 +33,16 @@ int main() {
         carta2, populacao2, turiticos2
     ;
 
-    char codigo1[10], codigo2[10]; // Coloquei um valor maior para evitar erro no teclado mais poderia ser[3]
+    char codigo1[10], codigo2[10];
 
     char estado1, estado2;
 
     char cidade1[50], cidade2[50];
 
-    float area1, area2, pib1, pib2;
+    float area1, area2, pib1, pib2,
+        densidade_pop1, densidade_pop2,
+        pib_per_capita1, pib_per_capita2        
+    ;
 
     printf("Bem vindo ao Super Trunfo\n \n"); // Mensagem de boas vindas
     printf("Jogador, cadastre sua primeira carta\n \n"); // Mensagem para o jogador cadastrar a sua primeira carta
@@ -63,13 +66,19 @@ int main() {
     printf("Digite a Area: ");
     scanf("%f", &area1);
 
-    printf("Digite o PIB: ");
+    printf("Digite o PIB: R$ ");
     scanf("%f", &pib1);
 
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &turiticos1);
 
     printf("Carta 1 cadastrada com sucesso!\n \n"); // Mensagem de cadastro com sucesso
+
+    // Calculando a densidade populacional da carta 1
+    densidade_pop1 = (float)populacao1 / area1; // Aqui foi feito a conversão explicita para float
+
+    // Calculando o PIB per capita da carta 1
+    pib_per_capita1 = pib1 / (float)populacao1; // Aqui foi feito a conversão explicita para float
 
     // Exibindo os dados da carta 1
     printf("Dados da Carta %d\n", carta1);
@@ -78,8 +87,10 @@ int main() {
     printf("Cidade: %s\n", cidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f Km²\n", area1);
-    printf("PIB: %.2f Bilhões de reais\n", pib1);
-    printf("Nº de Pontos Turisticos: %d\n \n", turiticos1);
+    printf("PIB R$ %.2f bilhões de reais\n", pib1);
+    printf("Nº de Pontos Turisticos: %d\n", turiticos1);
+    printf("Densidade Populacional: %.2f hab/Km²\n", densidade_pop1);
+    printf("PIB per Capita: R$ %.2f de reais\n \n", pib_per_capita1);
 
     printf("Jogador, cadastre a sua segunda carta\n \n"); // Mensagem para o jogador cadastrar a sua carta 2
 
@@ -102,13 +113,19 @@ int main() {
     printf("Digite a Area: ");
     scanf("%f", &area2);
 
-    printf("Digite o PIB: ");
+    printf("Digite o PIB: R$ ");
     scanf("%f", &pib2);
 
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &turiticos2);
 
     printf("Carta 2 cadastrada com sucesso\n \n"); // Mensagem de cadastro com sucesso
+
+    // Calculando a densidade populacional da carta 2
+    densidade_pop2 = (float)populacao2 / area2; // Aqui foi feito a conversão explicita para float
+
+    // Calculando o PIB per capita da carta 2
+    pib_per_capita2 = pib2 / (float)populacao2; // Aqui foi feito a conversão explicita para float
 
     // Exibindo os dados da carta 2
     printf("Dados da Carta %d\n", carta2);
@@ -117,8 +134,10 @@ int main() {
     printf("Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f Km²\n", area2);
-    printf("PIB: %.2f Bilhões de reais\n", pib2);
+    printf("PIB: R$ %.2f bilhões de reais\n", pib2);
     printf("Nº de Pontos Turisticos: %d\n \n", turiticos2);
+    printf("Densidade Populacional: %.2f hab/Km²\n", densidade_pop2);
+    printf("PIB per Capita: R$ %.2f de reais\n \n", pib_per_capita2);
 
     // Mensagem de Encerrando Jogo
     printf("Fim do Jogo! Aguarde Continuação.");
